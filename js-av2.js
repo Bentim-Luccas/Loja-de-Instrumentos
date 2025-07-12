ba061005f8f62df32d3dd4ae01de1872a56a155c
Luccas Bentim <luccasgtabr@gmail.com>
2025-07-12 14:36:27 -0300

    Primeiro commit deste projeto.

---
+++ js-av2.js
@@ -0,0 +1,29 @@
+class InstrumentoMusical {
+    constructor(nome, marca, tipo, cor, quantidadeEmEstoque) {
+        this.nome = nome;                // Ex: "Guitarra Les Paul"
+        this.marca = marca;              // Ex: "Gibson"
+        this.tipo = tipo;                // Ex: "Corda"
+        this.cor = cor;                  // Ex: "Vermelha"
+        this.quantidadeEmEstoque = quantidadeEmEstoque; // Atributo numérico
+    }
+
+    adicionarEstoque(valor) {
+        if (typeof valor === 'number' && valor >= 0) {
+            this.quantidadeEmEstoque += valor;
+            return { sucesso: true, mensagem: "Estoque atualizado com sucesso." };
+        }
+        return { sucesso: false, mensagem: "Valor inválido para adicionar ao estoque." };
+    }
+
+    removerEstoque(valor) {
+        if (typeof valor === 'number' && valor >= 0) {
+            if (this.quantidadeEmEstoque - valor >= 0) {
+                this.quantidadeEmEstoque -= valor;
+                return { sucesso: true, mensagem: "Estoque atualizado com sucesso." };
+            } else {
+                return { sucesso: false, mensagem: "Não é possível ter estoque negativo." };
+            }
+        }
+        return { sucesso: false, mensagem: "Valor inválido para reduzir o estoque." };
+    }
+}
